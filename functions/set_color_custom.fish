bbd881651740b8dac2b823717d93ce6166935f8c
Sjjad Hashemian <wolaws@gmail.com>
2016-04-18 12:15:52 +0430

    Add some colors

---
+++ functions/set_color_custom.fish
@@ -0,0 +1,10 @@
+function set_color_custom
+    set -U fish_color_valid_path            --underline
+    set -U fish_color_cwd                   white
+    set -U fish_color_cwd_root              white
+    set -U fish_color_match                 white
+    set -U fish_color_search_match          --background=000
+    set -U fish_color_selection             --background=000
+    set -U fish_pager_color_completion      white
+    set -U fish_pager_color_description     white
+end
